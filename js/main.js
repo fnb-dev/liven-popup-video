f59a0db7cb80678db83c28d9cdb76c893f2ed2ea
fnb-dev <ushahidul7@gmail.com>
2020-06-08 09:58:19 +0600

    Added html5 video with play function

--- js/main.js
+++ js/main.js
@@ -1,52 +1,35 @@
 (function ($) {
     $(function () {
-        var popupWrapper = $('#popup-wrap');
-        var popUp = $('#pop-up');
+        var videoWrapper = $('#video-wrapper');
+        var myVideo = $('#video').get(0); 
         
-        $(".popup-close, .drag-btn").click(function(e){
-            e.preventDefault();
-            
-            TweenMax.to(popupWrapper, 0.3, {
-                y: '100%',
-                ease: Sine.easeOut
+        $("#play-video").on('click touchstart', function (){
+            $(this).hide()
+            $('.popup-close').hide()
+            videoWrapper.fadeIn(400, function(){
+                playVideo();
             });
-            
-            TweenMax.to(popUp, .8, {
-                css: {
-                    opacity: 0,
-                    visibility: 'hidden'
-                },
-                delay: .5,
-                ease: Sine.easeOut
-            });
+        })
+        
+        myVideo.addEventListener('loadedmetadata', function() {
+            if (video.buffered.length === 0) return;
+
+            var bufferedSeconds = video.buffered.end(0) - video.buffered.start(0);
+            console.log(bufferedSeconds + ' seconds of video are ready to play!');
         });
         
+        myVideo.onended = (event) => {
+          setTimeout(function(){
+              videoWrapper.fadeOut(400, function(){
+                myVideo.currentTime = 0;  
+              });
+              $('.popup-close').show();
+              $("#play-video").show();
+          }, 1200)
+        };
         
-        
-        /*$('#popup-inner').draggable({
-            revert: true,
-            drag: function (e, ui) {
-                
-            },
-            revert: function (e, ui) {            
-                TweenMax.to(popupWrapper, 0.3, {
-                    y: '100%',
-                    ease: Sine.easeOut
-                });
-
-                TweenMax.to(popUp, .8, {
-                    css: {
-                        opacity: 0,
-                        visibility: 'hidden'
-                    },
-                    delay: .5,
-                    ease: Sine.easeOut
-                });
-                return !e;
-            }
-        });*/
-
-
-
+        function playVideo() { 
+           myVideo.play();
+        } 
     });
 })(jQuery);
